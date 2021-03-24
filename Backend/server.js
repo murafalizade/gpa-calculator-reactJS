beef8e82ddbbed1fbcdea99f8039ef47cebf4ebd
murafalizade <hmmuralizade29@gmail.com>
2021-03-24 18:52:03 +0400

    a

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,7 @@ app.use("/api/users", ResultRouter);
  app.get('/ping', function (req, res) {
    return res.send('pong');
  });
- app.get('/*', function (req, res) {
+ app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname, '../build', 'index.html'));
  });
 
