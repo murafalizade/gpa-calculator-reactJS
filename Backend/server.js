60a200a725362746cd80a2fcdb1651b8ecd675c3
murafalizade <hmmuralizade29@gmail.com>
2021-05-18 23:36:56 +0400

    github upload

--- Backend/server.js
+++ Backend/server.js
@@ -6,15 +6,10 @@ const app = express();
 const LoginRouter = require("./loginRouter");
 const ResultRouter = require("./resultRouter");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-})
+app.use(cors())
 
 app.use(favicon('../Client/build/favicon.ico'));
 app.use(express.static(__dirname));
